3e47410a28c63cb66c801ace401d201db972ee31
Alexander Fraltsov <kolonessens@yandex.ru>
2020-11-22 20:48:04 +0400

    fix: import path changed

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://ahnnkrl4r8.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://ahnnkrl4r8.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://1nit8x79k7.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://3oy44flzh3.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://ahnnkrl4r8.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
