01cae700eadf1d682d680a5982b7f6d009d2b333
AlexanderFraltsov <kolonessens@yandex.ru>
2020-12-06 16:37:23 +0400

    feat: remote changes added

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,9 +1,9 @@
-
 const API_PATHS = {
   product: 'https://ahnnkrl4r8.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://ahnnkrl4r8.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://3oy44flzh3.execute-api.eu-west-1.amazonaws.com/dev',
-  bff: 'https://ahnnkrl4r8.execute-api.eu-west-1.amazonaws.com/dev'
+  bff: 'https://ahnnkrl4r8.execute-api.eu-west-1.amazonaws.com/dev',
+  cart: 'http://alexander-fraltsov-cart-api-dev.eu-west-1.elasticbeanstalk.com/',
 };
 
 export default API_PATHS;
