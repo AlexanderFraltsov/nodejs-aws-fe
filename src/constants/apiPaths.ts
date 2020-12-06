f1b0aa81fff6197cce6ed78c22ca0afe767ea4e4
AlexanderFraltsov <kolonessens@yandex.ru>
2020-12-06 19:01:48 +0400

    fix: api path

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: 'https://ahnnkrl4r8.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://3oy44flzh3.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://ahnnkrl4r8.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'https://l4q73jpu28.execute-api.eu-west-1.amazonaws.com/dev/api',
+  cart: 'https://qdp1uvpx1d.execute-api.eu-west-1.amazonaws.com/dev/api',
 };
 
 export default API_PATHS;
