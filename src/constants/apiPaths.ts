2164026f084b2c0a11f7f0e47f1afa194e6c6a7a
AlexanderFraltsov <kolonessens@yandex.ru>
2020-12-06 17:05:49 +0400

    feat: add cart link on api gateway

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: 'https://ahnnkrl4r8.execute-api.eu-west-1.amazonaws.com/dev',
   import: 'https://3oy44flzh3.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://ahnnkrl4r8.execute-api.eu-west-1.amazonaws.com/dev',
-  cart: 'http://alexander-fraltsov-cart-api-dev.eu-west-1.elasticbeanstalk.com/',
+  cart: 'https://l4q73jpu28.execute-api.eu-west-1.amazonaws.com/dev/api',
 };
 
 export default API_PATHS;
